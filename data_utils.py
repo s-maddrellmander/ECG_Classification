a8b9e7482a15917ac9053c7fb8668c78b72850a9
Sam Maddrell-Mander <samuelm@graphcore.ai>
2022-11-30 11:41:35 +0000

    Collate Function covering the tensor manipulation

--- data_utils.py
+++ data_utils.py
@@ -7,8 +7,10 @@ from functools import partial
 
 class ECG_Dataset(Dataset):
     def __init__(self, data, targets):
-        self.data = torch.tensor(data).to(torch.float32)
-        self.targets = torch.IntTensor(targets)
+        # self.data = torch.tensor(data).to(torch.float32)
+        # self.targets = torch.LongTensor(targets)
+        self.data = data
+        self.targets = targets
         
     def __getitem__(self, index):
         x = self.data[index]
@@ -22,10 +24,14 @@ class ECG_Dataset(Dataset):
 def collate_fn(batch):
     """Collate Function to process each batch of the dataset
     Then any processing on the dataset that needs to be applied is done here
+    From the PyTorch Docs:
+    `After fetching a list of samples using the indices from sampler,
+    the function passed as the collate_fn
+    argument is used to collate lists of samples into batches.`
     """
-    # import ipdb; ipdb.set_trace()
-    # batch = [(torch.Tensor(t[0]), t[1]) for t in batch]
-    return tuple(zip(*batch))
+    data = torch.Tensor(np.array([b[0] for b in batch])).to(dtype=torch.float32)
+    target = torch.LongTensor(np.array([b[1] for b in batch]))
+    return data, target
 
 
 def preprocess_data(X, y, batch_size=1024):
@@ -35,10 +41,9 @@ def preprocess_data(X, y, batch_size=1024):
     
     dataset = ECG_Dataset(X, y)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True,
-                            collate_fn=None)
+                            collate_fn=collate_fn)
     logging.info(f"Loaded data into dataloader")
     train_features, train_labels = next(iter(dataloader))
-    # import ipdb; ipdb.set_trace()
-    print(f"Feature batch shape: {train_features.size()}")
-    print(f"Labels batch shape: {train_labels.size()}")
+    logging.info(f"Feature batch shape: {train_features.size()}")
+    logging.info(f"Labels batch shape: {train_labels.size()}")
     return dataloader
--- tests/test_pytorch_dataloader.py
+++ tests/test_pytorch_dataloader.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-from data_utils import preprocess_data, ECG_Dataset
+from data_utils import preprocess_data, ECG_Dataset, collate_fn
 
 @pytest.mark.parametrize("batch_size", ([5, 12, 16, 1024]))
 def test_preprocess_data(batch_size):
@@ -15,23 +15,39 @@ def test_preprocess_data(batch_size):
     assert type(dataloader) == torch.utils.data.dataloader.DataLoader
     assert dataloader.batch_size == batch_size
     for data in dataloader:
+        # assert type(data) == list
+        assert type(data[0]) == torch.Tensor
+        assert type(data[1]) == torch.Tensor
         assert data[0].shape == (batch_size, 100, 12)
         assert data[1].shape == (batch_size,)
 
 
 @pytest.mark.parametrize("batch_size", ([5]))
-@pytest.mark.parametrize("d_type", ([np.float32, np.float64]))
-def test_ECG_Dataset(batch_size, d_type):
-    X = np.random.random((batch_size*10, 100, 12)).astype(d_type)
+def test_ECG_Dataset(batch_size):
+    X = np.random.random((batch_size*10, 100, 12))
     y = np.random.randint(low=0, high=4, size=(batch_size*10))
     
     dataset = ECG_Dataset(X, y)
     assert dataset.data.shape == (batch_size * 10, 100, 12)
-    assert dataset.data.dtype == torch.float32
-    assert dataset.targets.dtype == torch.int32
+    # assert dataset.data.dtype == torch.float32
+    # assert dataset.targets.dtype == torch.int32
     assert dataset.targets.shape == (batch_size * 10, )
     # import ipdb; ipdb.set_trace()
     assert dataset[0][0].shape == (100, 12)
-    assert dataset[0][0].dtype == torch.float32
-    assert dataset[0][1].dtype == torch.int32
+
     assert dataset[0][1].shape == ()
+    
+@pytest.mark.parametrize("d_type", ([np.float32, np.float64]))
+def test_collate_fn(d_type):
+    # Inputs: batch - what is the batch object
+    # Input is a list of length batch size, containing tuples of tensors for the 
+    # dataset. In this case X, and y being (bs x seq_len x 12 channels)
+    # and the target a tensor of a single value
+    # Returns: The batch once processed - what format is that?
+    batch_size = 10
+    data = [(np.random.random((1, 100, 12)).astype(d_type), np.random.randint((1, ))) for _ in range(batch_size)]
+    outputs = collate_fn(data)
+    assert type(outputs) == tuple
+    assert outputs[0].dtype == torch.float32
+    assert outputs[1].dtype == torch.int64
+    
