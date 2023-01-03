2a927efac0373823268b571da0c1087ac80760aa
Sam Maddrell-Mander <samuelm@graphcore.ai>
2023-01-03 22:36:34 +0000

    Linting

--- tests/test_argparser.py
+++ tests/test_argparser.py
@@ -57,7 +57,5 @@ class TestParseCmdArgs:
         except SystemExit as error:
             # TODO: Check this is actually being run
             assert type(error) == SystemExit
-        
-        
 
     # TODO: Add a test for args and wandb as well - may be a seperate test location
